e3d2dde9f5dc841785673f7e763916ec20b51336
Sameer <111274488+sameerchug@users.noreply.github.com>
2025-08-05 06:33:50 +0530

    Update table-test.py
    Added infinite loop

--- table-test.py
+++ table-test.py
@@ -1,5 +1,7 @@
-table=input("Enter the number")
-table = int(table)
-for i in range(10):
-    print(i*table)
-input()
+while True:
+    table=input("Enter the number")
+    table = int(table)
+    for i in range(10):
+        print(i*table)
+    input()
+
